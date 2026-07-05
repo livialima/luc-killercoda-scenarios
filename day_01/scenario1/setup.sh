08e8b2715fbefec6baab62408bf6d9cb158f7edf
Livia Lima <livia.n.lima@gmail.com>
2026-07-04 21:01:57 -0300

    testing setup scripts

--- day_01/scenario1/setup.sh
+++ day_01/scenario1/setup.sh
@@ -1,10 +1,8 @@
 #!/usr/env bash
-
 #required for all lessons
 apt update
 apt upgrade -y
 apt autoremove
-
 #required for day1
 apt install ifstat -y
 apt install iftop -y
--- luc-server/setup.sh
+++ luc-server/setup.sh
@@ -1,31 +1,22 @@
-#!/usr/env bash
-
 #required for all lessons
 apt update
 apt upgrade -y
 apt autoremove -y
-
 #required for day_01
 apt install ifstat -y
 apt install iftop -y
-
 #required for day_02
 apt install snap snapd -y
 snap install tldr
-
 #required for day_04
 #hostnamectl set-hostname myserver
-
 #required for day_09
 apt install nmap -y
-
 #required for day_10
 apt install at -y
-
 #required for day_11
 apt install locate -y
 updatedb
-
 #required for day_17
 apt install build-essential -y
 
